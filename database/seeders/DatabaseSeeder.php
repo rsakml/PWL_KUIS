32f6ae833305ec1e551beb6f691fb87683a1a75f
= <dellajannata2112@gmail.com>
2022-03-16 20:06:06 +0700

    Update DatabaseSeeder call Barang dan Pelanggan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(SupplierSeeder::class);
         $this->call(PegawaiSeeder::class);
+        $this->call(BarangSeeder::class);
+        $this->call(PelangganSeeder::class);
     }
 }

7f882fab3020e45782ff20e178ca3569f5fd03b0
rsakml <rossaakmalia66@gmail.com>
2022-03-21 09:03:46 +0700

    update coloumn jenisKelamin PegawaiSeeder

--- database/seeders/PegawaiSeeder.php
+++ database/seeders/PegawaiSeeder.php
@@ -18,42 +18,49 @@ class PegawaiSeeder extends Seeder
             [
             'foto' => 'Amirul Anam.jpg',
             'nama' => 'Amirul Anam',
+            'jenisKelamin' => '0',
             'profesi' => 'Customer Service',
             'email' => 'amirul@gmail.com'
             ],
             [
             'foto' => 'Boy Candra.jpg',
             'nama' => 'Boy Candra',
+            'jenisKelamin' => '0',
             'profesi' => 'Maintener',
             'email' => 'boy@gmail.com'
             ],
             [
             'foto' => 'Ika Sari.jpg',
             'nama' => 'Ika Sari',
+            'jenisKelamin' => '1',
             'profesi' => 'Customer Service',
             'email' => 'ika@gmail.com'
             ],
             [
             'foto' => 'Bima Pratama.jpg',
             'nama' => 'Bima Pratama',
+            'jenisKelamin' => '0',
             'profesi' => 'Maintener',
             'email' => 'bima@gmail.com'
             ],
             [
             'foto' => 'Cindy Putri.jpg',
             'nama' => 'Cindy Putri',
+            'jenisKelamin' => '1',
             'profesi' => 'Customer Service',
             'email' => 'cindy@gmail.com'
             ],
             [
             'foto' => 'Violanda Dewi.jpg',
             'nama' => 'Violanda Dewi',
+            'jenisKelamin' => '1',
             'profesi' => 'Maintener',
             'email' => 'viola@gmail.com'
             ],
             [
             'foto' => 'Sekar Ayu.jpg',
             'nama' => 'Sekar Ayu',
+            'jenisKelamin' => '1',
             'profesi' => 'Maintener',
             'email' => 'sekar@gmail.com'
             ]

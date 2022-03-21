9847a524a29b9c9cdf403aa43f905294c088dd6e
rsakml <rossaakmalia66@gmail.com>
2022-03-21 10:21:18 +0700

    update coloumn kategori SupplierSeeder

--- database/seeders/SupplierSeeder.php
+++ database/seeders/SupplierSeeder.php
@@ -18,61 +18,61 @@ class SupplierSeeder extends Seeder
             [
             'gambar' => 'Niagahoster.png',
             'nama' => 'Niagahoster',
-            'jenis' => 'Hosting unlimited, Cloud hosting, Wordpress hosting, Email hosting, VPS, Pembuatan website, Domain',
+            'kategori' => 'Hosting unlimited, Cloud hosting, Wordpress hosting, Email hosting, VPS, Pembuatan website, Domain',
             'email' => 'niagahost@gmail.com'
             ],
             [
             'gambar' => 'Domainesia.png',
             'nama' => 'Domainesia',
-            'jenis' => 'Hosting, Cloud VPS, Domain',
+            'kategori' => 'Hosting, Cloud VPS, Domain',
             'email' => 'domainesia@gmail.com'
             ],
             [
             'gambar' => 'Ardetamedia.png',
             'nama' => 'Ardetamedia',
-            'jenis' => 'Unlimited hosting, Semi dedicated hosting, Cloud hosting, VPS, Dedicated server, Email, Domain',
+            'kategori' => 'Unlimited hosting, Semi dedicated hosting, Cloud hosting, VPS, Dedicated server, Email, Domain',
             'email' => 'ardetamedia@gmail.com'
             ],
             [
             'gambar' => 'Warnahost.png',
             'nama' => 'Warnahost',
-            'jenis' => 'Hosting unlimited, Wordpres hosting, Cloud hosting, VPS, Dedicated server, Jasa managed server, Jasa pembuatan web, Domain',
+            'kategori' => 'Hosting unlimited, Wordpres hosting, Cloud hosting, VPS, Dedicated server, Jasa managed server, Jasa pembuatan web, Domain',
             'email' => 'warnahost@gmail.com'
             ],
             [
             'gambar' => 'Idcloudhost.png',
             'nama' => 'Idcloudhost',
-            'jenis' => 'Cloud hosting, Wordpress hosting, Cloud VPS, Dedicated server, Colocation server, Domain',
+            'kategori' => 'Cloud hosting, Wordpress hosting, Cloud VPS, Dedicated server, Colocation server, Domain',
             'email' => 'idCloud@gmail.com'
             ],
             [
             'gambar' => 'Jetorbit.png',
             'nama' => 'Jetorbit',
-            'jenis' => 'Shared hosting, VPS, Wordpress hosting, Email hosting, Dedicated server, Domain',
+            'kategori' => 'Shared hosting, VPS, Wordpress hosting, Email hosting, Dedicated server, Domain',
             'email' => 'jetorbit@gmail.com'
             ],
             [
             'gambar' => 'Dracoola Multimedia.png',
             'nama' => 'Dracoola Multimedia',
-            'jenis' => 'Shared hosting, Domain',
+            'kategori' => 'Shared hosting, Domain',
             'email' => 'dracoola@gmail.com'
             ],
             [
             'gambar' => 'Nusantarahost.png',
             'nama' => 'Nusantarahost',
-            'jenis' => 'Shared hosting, E-Learning hosting, VPS, Dedicated server, Domain',
+            'kategori' => 'Shared hosting, E-Learning hosting, VPS, Dedicated server, Domain',
             'email' => 'nusantara@gmail.com'
             ],
             [
             'gambar' => 'Jogjahost.png',
             'nama' => 'Jogjahost',
-            'jenis' => 'Shared hosting, Semi dedicated hosting, Dedicated hosting, VPS, Dedicated server, E-learning CBT, Domain',
+            'kategori' => 'Shared hosting, Semi dedicated hosting, Dedicated hosting, VPS, Dedicated server, E-learning CBT, Domain',
             'email' => 'jogjahost@gmail.com'
             ],
             [
             'gambar' => 'Domosquare.png',
             'nama' => 'Domosquare',
-            'jenis' => 'Shared hosting, VPS, Dedicated server, Domain',
+            'kategori' => 'Shared hosting, VPS, Dedicated server, Domain',
             'email' => 'domosquare@gmail.com'
             ]
 
